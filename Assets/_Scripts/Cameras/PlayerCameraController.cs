35eed4e7ea2905c0863fd274badbd4ec015f7444
Łukasz Gretka <selectuss@gmail.com>
2018-06-03 21:04:17 +0200

    Add basic multiplayer system, Add temporary gameobject for 3 character classes Implementation of player movement Set basic camera script

---
+++ Assets/_Scripts/Cameras/PlayerCameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public float CameraSmoothness = 2f;
+
+    private Camera mainCamera;
+    private Transform playerTransform;
+    private Vector3 cameraOffset;
+
+
+    private void Awake ()
+    {
+        playerTransform = GetComponent<Transform>();
+        mainCamera = Camera.main;
+    }
+
+    private void Start()
+    {
+        cameraOffset = playerTransform.transform.position - mainCamera.transform.position;
+    }
+
+    private void Update ()
+    {
+        mainCamera.transform.position = CalculateCameraPossition();
+    }
+
+    private Vector3 CalculateCameraPossition(bool lerping = true)
+    {
+        return lerping ? Vector3.Lerp(mainCamera.transform.position, transform.position - cameraOffset, CameraSmoothness * Time.deltaTime)
+                       : transform.position - cameraOffset;
+    }
+}
--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerMovement : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : NetworkBehaviour
+{
+    protected float MovementSpeed = 5f;
+    protected float RotationSpeed = 100f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody playerRigidbody;
+    private Vector3 playerMovementVector;
+    private LayerMask groundLayerMask;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        groundLayerMask = LayerMask.GetMask("Ground");
+    }
+
+    private void FixedUpdate ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        playerMovementVector = GetUserRawInputNormalized() * Time.deltaTime * MovementSpeed;
+        playerRigidbody.MovePosition(this.transform.position + playerMovementVector);
+        playerRigidbody.MoveRotation(GetPlayerRotationByMousePoint());
+    }
+
+    private Vector3 GetUserRawInputNormalized()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        playerMovementVector.Set(horizontalInput, 0f, verticalInput);
+        return playerMovementVector.normalized;
+    }
+
+    private Quaternion GetPlayerRotationByMousePoint()
+    {
+        Ray rayFromCam = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(rayFromCam, out rayHit, Camera.main.farClipPlane, groundLayerMask))
+        {
+            Vector3 rotationVector = rayHit.point - transform.position;
+            rotationVector.y = default(float);
+            return Quaternion.LookRotation(rotationVector);
+        }
+
+        return this.transform.rotation;
+    }
+
 }
